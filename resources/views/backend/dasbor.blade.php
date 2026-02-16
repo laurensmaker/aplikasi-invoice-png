ca0f90f8f2b58432e5a1564ae306d97a63ca39f1
laurensmaker <laurenkostan@gmail.com>
2026-02-17 03:23:20 +0900

    setup projeck dan slicing template

---
+++ resources/views/backend/dasbor.blade.php
@@ -0,0 +1,61 @@
+@extends('backend.layouts.main')
+@section('content')
+    <div class="row">
+        <div class="col-xxl-10">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="stats-box card bg-white border-0 rounded-10 mb-4">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between align-items-center mb-3 pb-1">
+                                <div class="flex-grow-1 me-3">
+                                    {{-- <h3 class="body-font fw-bold fs-3 mb-2">{{ $totalEksportir }}</h3> --}}
+                                    <span>Total Eksportir</span>
+                                </div>
+                                <div class="flex-shrink-0">
+                                    <div class="icon transition">
+                                        <i class="flaticon-donut-chart"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- <div class="d-flex align-items-center">
+                                <div class="svg-success me-2">
+                                    <i data-feather="trending-up"></i>
+                                </div>
+                                <p class="fw-semibold"><span class="text-success">1.3%</span> Up from past week
+                                </p>
+                            </div> --}}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-sm-6">
+                    <div class="stats-box card bg-white border-0 rounded-10 mb-4">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between align-items-center mb-3 pb-1">
+                                <div class="flex-grow-1 me-3">
+                                    {{-- <h3 class="body-font fw-bold fs-3 mb-2">{{ $totalPengajuan }}</h3> --}}
+                                    <span>Total Pengajuan</span>
+                                </div>
+                                <div class="flex-shrink-0">
+                                    <div class="icon transition">
+                                        <i class="flaticon-goal"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- <div class="d-flex align-items-center">
+                                <div class="svg-danger me-2">
+                                    <i data-feather="trending-down"></i>
+                                </div>
+                                <p class="fw-semibold"><span class="text-danger">1.3%</span> Down from past week
+                                </p>
+                            </div> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+   
+    
+@endsection
---
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,99 @@
+ <div class="sidebar-area" id="sidebar-area">
+        <div class="logo position-relative">
+            <a href="index.html" class="d-block text-decoration-none">
+                <img src="{{ asset('backend/assets/images/pngcs-logo.png') }}" width="80" alt="logo-icon">
+                {{-- <span class="logo-text fw-bold text-dark">Farol</span> --}}
+            </a>
+            <button
+                class="sidebar-burger-menu bg-transparent p-0 border-0 opacity-0 z-n1 position-absolute top-50 end-0 translate-middle-y"
+                id="sidebar-burger-menu">
+                <i data-feather="x"></i>
+            </button>
+        </div>
+        <aside id="layout-menu" class="layout-menu menu-vertical menu active" data-simplebar>
+            <ul class="menu-inner">
+                <li class="menu-item">
+                    <a href="{{ route('dasbor') }}" class="menu-link">
+                        <i data-feather="grid" class="menu-icon tf-icons"></i>
+                        <span class="title">Dasbor</span>
+                    </a>
+                </li>
+                <li class="menu-title small text-uppercase">
+                    <span class="menu-title-text">DATA</span>
+                </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="user" class="menu-icon tf-icons"></i>
+                            <span class="title">Data User</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="globe" class="menu-icon tf-icons"></i>
+                            <span class="title">Data Negara</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="compass" class="menu-icon tf-icons"></i>
+                            <span class="title">Data Provinsi</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="minus-square" class="menu-icon tf-icons"></i>
+                            <span class="title">Data Kabupaten</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:void(0);" class="menu-link menu-toggle active">
+                            <i data-feather="columns" class="menu-icon tf-icons"></i>
+                            <span class="title">Kantor Pabean</span>
+                        </a>
+                        <ul class="menu-sub">
+                            <li class="menu-item">
+                                <a href="" class="menu-link">
+                                    Kategori Kantor Pabean
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="" class="menu-link">
+                                    Kantor Pabean
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="pocket" class="menu-icon tf-icons"></i>
+                            <span class="title">Cara Perdagangan</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="user" class="menu-icon tf-icons"></i>
+                            <span class="title">Cara Pembayaran</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="user" class="menu-icon tf-icons"></i>
+                            <span class="title">Kategori Ekspor</span>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link">
+                            <i data-feather="user" class="menu-icon tf-icons"></i>
+                            <span class="title">Jenis Ekspor</span>
+                        </a>
+                    </li>
+                
+                
+               
+                
+              
+                
+            </ul>
+        </aside>
+       
+    </div>
